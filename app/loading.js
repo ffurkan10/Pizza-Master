30dec691f26b51987ef2a0d3cf6e1a2c1ace22fd
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2023-10-16 17:34:15 +0300

    add loading

---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import cn from "classnames";
+const Loading = () => {
+  return (
+    <div className="loading open">
+      <div className="loader"></div>
+    </div>
+  );
+};
+export default Loading;
